8d72e05b53ed4f5be5ab9b490fc867bc07c5afe6
gustavo-duarte-67 <gustavo_d_lopes@estudante.sesisenai.org.br>
2026-09-21 20:35:37 -0300

    Cria aplicativo

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("================================");
+console.log("       MEU APLICATIVO");
+console.log("================================");
+console.log(" ");
+console.log("Nome: Gustavo Duarte Lopes");
+console.log("Cidade: Schroeder");
+console.log("Atividade de interesse: jogos");
+console.log("Quero aprender: desenvolvimento de aplicativos");
+console.log("Área de interesse: tecnologia");
+console.log("Mensagem: Estou evoluindo na programação!");
