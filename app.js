8ccf7d2fc971f9acfa6d238dc875876595926435
gustavo-duarte-67 <gustavo_d_lopes@estudante.sesisenai.org.br>
2026-09-21 21:00:57 -0300

    Adiciona novo desafio

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ console.log("Nome: Gustavo Duarte Lopes");
 console.log("Cidade: Schroeder");
 console.log("Atividade de interesse: jogos");
 console.log("Quero aprender: desenvolvimento de aplicativos ");
-console.log("Área de interesse: tecnologia");
+console.log("Área de interesse: tecnologia ");
 console.log("Mensagem: Estou evoluindo na programação! ");
